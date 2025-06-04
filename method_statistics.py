d04dc80e234d21ed8e12df5cd4e484f0abe0c70a
Petr Hanák <pe.hanak.tr@gmail.com>
2025-06-04 09:54:33 +0200

    method statistics for JC API 3.2.0

--- method_statistics.py
+++ method_statistics.py
@@ -3,54 +3,77 @@ import re
 from urllib.parse import unquote
 import csv
 
-DOCS_URL = "https://docs.oracle.com/en/java/javacard/3.1/jc_api_srvc/api_classic/"
+DOCS_URL_31 = "https://docs.oracle.com/en/java/javacard/3.1/jc_api_srvc/api_classic/"
+DOCS_URL_32 = "https://docs.oracle.com/en/java/javacard/3.2/jcapi/api_classic/"
 
 
-def get_full_webpage(package_name: str, class_name: str) -> str:
-    url_end = package_name.replace('.', '/') + "/" + class_name + ".html"
-    response = requests.get(DOCS_URL + url_end)
+def load_webpage(url: str) -> str:
+    response = requests.get(url)
     if response.status_code != 200:
         print("Couldn't load documentation")
         exit()
     return response.text
 
 
-def find_method_parameters(webpage: str, method_name: str) -> list[str] | None:
-    # print(webpage)
-    match = re.search(fr'#{method_name}\((.*?)\)', webpage)
-    if match:
-        return unquote(match.group(1)).split(',')
-    print(f"Method {method_name} not found")
-    return None
+def get_package_list() -> list[str]:
+    webpage = load_webpage(DOCS_URL_32 + "index.html")
+
+    match = re.findall(r'<a href="(.*?)/package-summary.html">', webpage)
+    return list(set(match))
 
 
-def get_method_parameters(package_name: str, class_name: str, method_name: str) -> list[str] | None:
-    webpage = get_full_webpage(package_name, class_name)
-    return find_method_parameters(webpage, method_name)
+def get_class_list(package_name: str) -> list[str]:
+    webpage = load_webpage(f"{DOCS_URL_32}/{package_name}/package-summary.html")
+
+    match = re.findall(r'<a href="([^/]*?).html" title', webpage)
+    return list(set(match))
 
 
-def convert_parameters(parameters: list[str]) -> str:
-    return f"({';'.join(parameters)})"
+def get_method_list(package_name: str, class_name: str) -> list[str] | None:
+    webpage = load_webpage(f"{DOCS_URL_32}/{package_name}/{class_name}.html")
 
-# parameters = get_method_parameters("javacard.framework", "PIN", "getTriesRemaining")
+    match = re.findall(r'#([a-zA-Z]*?)\(.*?\)">', webpage)
+    return list(set(match))
 
-input_filename = "overview_table.csv"
-output_filename = "method_statistics.csv"
 
-with open(input_filename, "r") as f:
-    csv_reader = csv.reader(f)
-    entries = [row for row in csv_reader]
+def get_method_signatures(package_name: str, class_name: str, method_name: str) -> list[str] | None:
+    webpage = load_webpage(f"{DOCS_URL_32}/{package_name}/{class_name}.html")
 
-for entry in entries:
-    if len(entry) == 6:
-        parameters = get_method_parameters(entry[1], entry[3], entry[5])
-        parameters = parameters if parameters is not None else 'ERROR'
-        print(entry[1], entry[3], entry[5], parameters)
-        entry.append(convert_parameters(parameters))
+    matches = re.findall(fr'#{method_name}(\(.*?\))', webpage)
+    if not matches:
+        return []
 
-with open(output_filename, "w") as f:
-    csv_writer = csv.writer(f)
-    csv_writer.writerow(["AID", "package name", "class token", "class name", "method token", "method name", "method parameters"])
-    for row in entries:
-        csv_writer.writerow(row)
+    signatures = []
+    for match in matches:
+        signature = unquote(match)
+        signature = signature.replace(",", ";")
+        signatures.append(signature)
+    return list(set(signatures))
 
+
+csv_file = open("overview_table_320_new.csv", "w")
+csv_writer = csv.writer(csv_file)
+
+csv_writer.writerow(["package name", "class name", "method name", "method signature"])
+
+packages = get_package_list()
+for package in packages:
+    classes = get_class_list(package)
+    if not classes:
+        continue
+    for class_name in classes:
+        methods = get_method_list(package, class_name)
+        if not methods:
+            print(package, class_name)
+            csv_writer.writerow([package.replace("/", "."), class_name])
+            continue
+        for method in methods:
+            if method == 'equals':
+                continue
+            signatures = get_method_signatures(package, class_name, method)
+            if not signatures:
+                print(f"ERROR. Signature for method {method}, package {package}, class {class_name} not found")
+                exit()
+            for signature in signatures:
+                print(package, class_name, method, signature)
+                csv_writer.writerow([package.replace("/", "."), class_name, method, signature])
