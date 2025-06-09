adcce42b82aa8a5f961828f37cede0008245bffe
Petr Hanák <pe.hanak.tr@gmail.com>
2025-06-09 11:57:49 +0200

    fix repeater

--- simple_repeater.py
+++ simple_repeater.py
@@ -4,4 +4,4 @@ package_name = "javacardx_crypto"
 i = 0
 
 while True:
-    subprocess.run(["java", "-jar", "gp.jar", "--install", f"test_{package_name}_{i}.cap"], stdout=subprocess.PIPE)
+    subprocess.run(["java", "-jar", "gp.jar", "--install", f"templates_ff/test_{package_name}_{i}.cap"])
