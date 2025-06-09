cff62ce990bf4f3368101ebf413f6ff0eb911a0c
Petr Hanák <pe.hanak.tr@gmail.com>
2025-06-09 12:20:58 +0200

    add delay

--- simple_repeater.py
+++ simple_repeater.py
@@ -1,7 +1,9 @@
 import subprocess
+import time
 
 package_name = "javacardx_crypto"
 i = 0
 
 while True:
     subprocess.run(["java", "-jar", "gp.jar", "--install", f"templates_ff/test_{package_name}_{i}.cap"])
+    time.sleep(1)
