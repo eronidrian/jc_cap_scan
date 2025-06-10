66bfd4a6ec146bd10969202ddd3fde70016d203c
Petr Hanák <pe.hanak.tr@gmail.com>
2025-06-10 19:40:56 +0200

    add uninstall

--- simple_repeater.py
+++ simple_repeater.py
@@ -5,5 +5,7 @@ package_name = "javacardx_crypto"
 i = 0
 
 while True:
-    subprocess.run(["java", "-jar", "gp.jar", "--install", f"templates_ff/test_{package_name}_{i}.cap"])
+    subprocess.run(["java", "-jar", "gp.jar", "--install", f"templates_ff/test_{package_name}_{i}.cap", "-d"])
     time.sleep(1)
+    subprocess.run(["java", "-jar", "gp.jar", "--uninstall",
+                    f"templates_ff/test_{package_name}_{i}.cap"])
