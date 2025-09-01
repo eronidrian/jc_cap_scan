c99d12d2ee5dcddeefdc7b44860da7f755d8cb08
Petr Hanák <pe.hanak.tr@gmail.com>
2025-09-01 17:18:09 +0200

    add CLI for trs analyser

--- trs_analyser.py
+++ trs_analyser.py
@@ -1,18 +1,25 @@
+import argparse
+import os
+
 import trsfile
 import csv
 import numpy as np
 import numba
+from numba.np.arrayobj import numpy_broadcast_arrays
 
-MEASUREMENTS_FILENAME = "time_measurements.csv"
-NUM_OF_TRACES_IN_FILE = 10
-NUM_OF_CHANGED_BYTES = 11
+MAX_GAP = 300_000
+MIN_DURATION = 150_000
+THRESHOLD_HIGH = 6
 
-PACKAGE_NAME = "javacardx_framework_util_intx"
-CHANGED_BYTE = "ff"
+TRACE_TO_EXTRACT = -1
 
 def extract_trace(filename: str, trace_index: int) -> list[int]:
     with trsfile.open(filename, 'r') as traces:
         return traces[trace_index]
+
+def get_num_of_traces(filename: str) -> int:
+    with trsfile.open(filename, 'r') as traces:
+        return len(traces)
 
 
 @numba.njit
@@ -36,7 +43,7 @@ def merge_gaps(starts, ends, max_gap):
 
     return result_starts, result_ends
 
-def find_high_consumption_periods(data, threshold, min_duration, max_gap):
+def find_high_consumption_periods(data, threshold=THRESHOLD_HIGH, min_duration=MIN_DURATION, max_gap=MAX_GAP):
     data = np.asarray(data)
     high = data > threshold
 
@@ -63,67 +70,79 @@ def find_high_consumption_periods(data, threshold, min_duration, max_gap):
     return list(zip(starts[valid], ends[valid] - 1))
 
 
-def bulk_process(traces_dirname: str, max_gap: int, min_duration: int, threshold_high: int):
-    csv_file = open(MEASUREMENTS_FILENAME, "w")
+def bulk_extract(traces_dirname: str, output_filename: str) -> tuple[int, int]:
+    csv_file = open(output_filename, "w")
     csv_writer = csv.writer(csv_file)
+    num_of_files = len(os.listdir(traces_dirname))
 
-    for byte_changed in range(NUM_OF_CHANGED_BYTES):
-        print(f"{byte_changed + 1}/{NUM_OF_CHANGED_BYTES}")
-        for trace_num in range(NUM_OF_TRACES_IN_FILE):
-            print(f"{trace_num + 1}/{NUM_OF_TRACES_IN_FILE}")
-            filename = f'{traces_dirname}/all_traces_{PACKAGE_NAME}_{byte_changed}_{CHANGED_BYTE}.trs'
-            trace = extract_trace(filename, trace_num)
+    for file_num, trs_file_name in enumerate(os.listdir(traces_dirname)):
+        print(f"{file_num + 1}/{num_of_files}")
+        trs_path = os.path.join(traces_dirname, trs_file_name)
+        num_of_traces_in_file = get_num_of_traces(trs_path)
+        for trace_num in range(num_of_traces_in_file):
+            print(f"{trace_num + 1}/{num_of_traces_in_file}")
 
-            periods = find_high_consumption_periods(trace, threshold_high, min_duration, max_gap)
+            trace = extract_trace(trs_path, trace_num)
+
+            periods = find_high_consumption_periods(trace)
             times = [period[1] - period[0] for period in periods]
 
             print(periods)
             print(times)
-            csv_writer.writerow([byte_changed, trace_num] + times)
+            csv_writer.writerow([file_num, trace_num] + times)
         csv_writer.writerow([])
 
     csv_file.close()
+    return num_of_files, num_of_traces_in_file
 
 
-def extract_single_response(response_index: int, output_filename: str) -> None:
-    csv_file_read = open(MEASUREMENTS_FILENAME, "r")
+def extract_single_response(response_index: int, input_filename: str, output_filename: str, num_of_files: int, number_of_traces_in_file: int) -> None:
+    csv_file_read = open(input_filename, "r")
     csv_reader = csv.reader(csv_file_read)
 
     csv_file_write = open(output_filename, "w")
     csv_writer = csv.writer(csv_file_write)
 
-    csv_writer.writerow(["modification"] + [i for i in range(1, NUM_OF_TRACES_IN_FILE + 1)])
+    csv_writer.writerow(["modification"] + [i for i in range(1, num_of_traces_in_file + 1)])
     rows = [row for row in csv_reader if row]
 
-    row_names = [f"AID {i}. byte" for i in range(1, NUM_OF_CHANGED_BYTES - 1)]
+    row_names = [f"AID {i}. byte" for i in range(1, num_of_files - 1)]
     row_names.extend([
         "Major version",
         "Minor version",
     ])
 
-    for byte_changed in range(NUM_OF_CHANGED_BYTES):
+    for byte_changed in range(num_of_files):
         new_row = []
-        for measurement in range(NUM_OF_TRACES_IN_FILE):
-            new_row.append(rows[byte_changed * NUM_OF_TRACES_IN_FILE + measurement][response_index])
+        for measurement in range(num_of_traces_in_file):
+            new_row.append(rows[byte_changed * num_of_traces_in_file + measurement][response_index])
         csv_writer.writerow([row_names[byte_changed]] + new_row)
 
     csv_file_write.close()
     csv_file_read.close()
 
 
-max_gap = 300_000
-min_duration = 150_000
-threshold_high = 6
-results_dirname = f'/home/petr/Downloads/diplomka/side_channel_measurement/results/new/{PACKAGE_NAME}_{CHANGED_BYTE}'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog="TRS analyser",
+        description=""
+    )
 
-bulk_process(results_dirname, max_gap, min_duration, threshold_high)
+    parser.add_argument('-r', '--results_dirname', help='Directory with the TRS files', required=True)
+    parser.add_argument('-o', '--output_filename', help='Filename for the complete CSV', required=True)
+    parser.add_argument('-e', '--extract_filename', help='Filename for the CSV with extracted time', required=True)
 
-# extract_single_response(-1, f"aid_upload_times_{PACKAGE_NAME}_last.csv")
+
+    args = parser.parse_args()
+
+    num_of_files, num_of_traces_in_file = bulk_extract(args.results_dirname, args.output_filename)
+
+    extract_single_response(-1, args.output_filename, args.extract_filename, num_of_files, num_of_traces_in_file)
 
 # result_file = open("all.csv", "w")
 # csv_writer = csv.writer(result_file)
 # csv_writer.writerow(["modification"] + [i for i in range(1, 41)])
-# row_names = [f"AID {i}. byte" for i in range(1, NUM_OF_CHANGED_BYTES - 1)]
+# row_names = [f"AID {i}. byte" for i in range(1, NUM_OF_TRS_FILES - 1)]
 # row_names.extend([
 #     "Major version",
 #     "Minor version",
