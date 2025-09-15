c8d4034e7f56171b751d68812234320ab58dbb54
Petr Hanák <pe.hanak.tr@gmail.com>
2025-09-15 13:19:38 +0200

    make it less simple

--- simple_repeater.py
+++ simple_repeater.py
@@ -3,9 +3,16 @@ import time
 
 package_name = "javacardx_crypto"
 i = 0
+counter = 0
 
 while True:
-    subprocess.run(["java", "-jar", "gp.jar", "--install", f"templates_ff/test_{package_name}_{i}.cap", "-d"])
+    print(counter)
+    subprocess.run(["java", "-jar", "gp.jar", "--install", f"templates_ff/test_{package_name}_{i}.cap"])
     time.sleep(1)
-    subprocess.run(["java", "-jar", "gp.jar", "--uninstall",
-                    f"templates_ff/test_{package_name}_{i}.cap"])
+    if counter % 10 == 0:
+        subprocess.run(["java", "-jar", "gp.jar", "--install", f"templates_ff/test_{package_name}_9.cap"])
+        subprocess.run(["java", "-jar", "gp.jar", "--uninstall", f"templates_ff/test_{package_name}_9.cap"])
+
+    # subprocess.run(["java", "-jar", "gp.jar", "--uninstall",
+    #                 f"templates_ff/test_{package_name}_{i}.cap"])
+    counter += 1
