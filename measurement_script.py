83b93026703990c921948c8d58cca49e6ccaff89
Petr Hanák <pe.hanak.tr@gmail.com>
2025-10-01 09:56:32 +0200

    prepare for correct installation measurements

--- measurement_script.py
+++ measurement_script.py
@@ -29,7 +29,7 @@ VALID_CAP_FILE_PATH = "templates_ff/test_javacardx_crypto_9.cap"
 
 PACKAGE_NAME = "javacardx_crypto"
 CHANGED_BYTE_VALUE = "ff"
-BYTE_RANGE = list(range(9))
+BYTE_RANGE = [9]
 
 
 # PicoScope setup and capture functions
@@ -158,6 +158,8 @@ def run_installation_and_capture(chandle, status, trs_writer, changed_byte, pack
     # Wait for capture to complete
     capture_done_event.wait()
 
+    uninstall_package(changed_byte_value, package_name, changed_byte_value)
+
 
 def main():
     start_time = time.time()  # Start timing the program
@@ -186,8 +188,8 @@ def main():
     }
 
     try:
-        for package_name in ["javacard_security", "javacardx_crypto"]:
-            for changed_byte_value in ["ee", "ff"]:
+        for package_name in ["javacard_security"]:
+            for changed_byte_value in ["ee"]:
                 random_range = BYTE_RANGE
                 random.shuffle(random_range)
                 for changed_byte in random_range:
@@ -205,10 +207,6 @@ def main():
                             run_installation_and_capture(chandle, status, trs_writer, changed_byte, package_name,
                                                          changed_byte_value, index, folder=folder_name)
 
-                            if index % FAULT_COUNTER_RESET == 0:
-                                print("Resetting fault counter...")
-                                reset_fault_counter()
-                                # reset fault counter
                     time.sleep(10)
                     print()
 
