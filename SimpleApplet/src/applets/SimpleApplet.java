df32118e0a5b5f10af0eab57d7c8a88334273d4d
Petr Hanák <pe.hanak.tr@gmail.com>
2025-11-12 17:57:33 +0100

    testing apdu method 3

---
+++ SimpleApplet/src/applets/SimpleApplet.java
@@ -0,0 +1,33 @@
+package applets ;
+import javacard.framework.*;
+import javacardx.crypto.*;
+
+public class SimpleApplet extends javacard.framework.Applet {
+    private Cipher m_encryptCipher = null ;
+    protected SimpleApplet(byte[] buffer, short offset, byte length){
+        // register this instance
+        m_encryptCipher = Cipher.getInstance(Cipher.ALG_DES_CBC_NOPAD, false);
+        Check_static();
+        register();
+    }
+
+    public static void install(byte[] bArray, short bOffset, byte bLength) throws ISOException {
+        // applet instance creation
+        new SimpleApplet(bArray, bOffset, bLength);
+    }
+
+    public boolean select() {
+        return true ;
+    }
+
+    public void process (APDU apdu) throws ISOException {
+        byte[] apduBuffer = apdu.getBuffer();
+        if (selectingApplet()) {
+            return ;
+        }
+    }
+
+    public void Check_static(){
+        APDU.waitExtension();
+    }
+    }
--- method_test.py
+++ method_test.py
@@ -1,10 +1,11 @@
 import csv
+import re
 import shutil
 import subprocess
 import os
 
 auth = []
-auth = ["-key", "404142434445464748494A4B4C4D4E4F404142434445464748494A4B4C4D4E4F"]
+# auth = ["-key", "404142434445464748494A4B4C4D4E4F404142434445464748494A4B4C4D4E4F"]
 
 def uninstall_package(cap_file_name):
     return subprocess.run(["java", "-jar", "gp.jar", "--uninstall",
@@ -34,52 +35,65 @@ def install_package(cap_file_name) -> str:
 
     return message
 
-f = open("javacos_a_40.csv", "w")
-csv_writer = csv.writer(f)
-
-for method_token in range(255):
-    f = open(os.path.join('template_method', 'applets', 'javacard', 'ConstantPool.cap'), 'rb')
-    hexdata = f.read().hex().upper()
-    f.close()
-    hex_array = bytearray(bytes.fromhex(hexdata))
-
-    hex_array[-1] = int(method_token)
-    f = open(os.path.join('template_method', 'applets', 'javacard', 'ConstantPool.cap'), 'wb')
-    f.write(hex_array)
-    f.close()
-
-    # create new cap file by zip of directories
-    cap_name = f'test_{method_token}.cap'
-    shutil.make_archive(cap_name, 'zip', os.path.join('template_method'))
-
-    # remove zip suffix
-    if os.path.exists(cap_name):
-        os.remove(cap_name)
-    os.rename(f'{cap_name}.zip', cap_name)
-
-    result = install_package(cap_name)
-    print(f"{method_token} - {result}")
-    csv_writer.writerow([method_token, result])
-
-    os.remove(cap_name)
-
-    if method_token % 5 == 0:
-        print("Resetting fault counter...")
-        install_package("good_package.cap")
-
-
-# f = open("infineon_secora.csv", "r")
-# csv_reader = csv.reader(f)
+# f = open("javacos_a_40.csv", "w")
+# csv_writer = csv.writer(f)
 #
-# f_1 = open("infineon_secora_categories.csv", "w")
-# csv_writer = csv.writer(f_1)
+# for method_token in range(255):
+#     f = open(os.path.join('template_method', 'applets', 'javacard', 'ConstantPool.cap'), 'rb')
+#     hexdata = f.read().hex().upper()
+#     f.close()
+#     hex_array = bytearray(bytes.fromhex(hexdata))
 #
-# for line in csv_reader:
-#     if "Failed to communicate" in line[1]:
-#         return_code = 1
-#     elif "LOAD failed: 0x6A80" in line[1]:
-#         return_code = 2
-#     else:
-#         return_code = 0
-#     csv_writer.writerow([line[0], return_code])
-#     # print(line[0])
+#     hex_array[-1] = int(method_token)
+#     f = open(os.path.join('template_method', 'applets', 'javacard', 'ConstantPool.cap'), 'wb')
+#     f.write(hex_array)
+#     f.close()
+#
+#     # create new cap file by zip of directories
+#     cap_name = f'test_{method_token}.cap'
+#     shutil.make_archive(cap_name, 'zip', os.path.join('template_method'))
+#
+#     # remove zip suffix
+#     if os.path.exists(cap_name):
+#         os.remove(cap_name)
+#     os.rename(f'{cap_name}.zip', cap_name)
+#
+#     result = install_package(cap_name)
+#     print(f"{method_token} - {result}")
+#     csv_writer.writerow([method_token, result])
+#
+#     os.remove(cap_name)
+#
+#     if method_token % 5 == 0:
+#         print("Resetting fault counter...")
+#         install_package("good_package.cap")
+
+card_name = "javacos_a_40"
+f = open(f"{card_name}.csv", "r")
+csv_reader = csv.reader(f)
+
+f_1 = open(f"{card_name}_categories.csv", "w")
+csv_writer = csv.writer(f_1)
+
+categories = []
+
+for line in csv_reader:
+    method_token = line[0]
+    message = line[1].strip()
+    message = re.sub(r'\{[0-9a-f]*\}', r'{<hex>}', message)
+
+    if message in categories:
+        category_code = categories.index(message) + 1
+    elif message == "CAP loaded":
+        category_code = 0
+    else:
+        categories.append(message)
+        category_code = categories.index(message) + 1
+
+    csv_writer.writerow([method_token, category_code])
+
+print("Categories:")
+print(f"CAP loaded")
+for i, category in enumerate(categories):
+    print(f"{category}")
+
