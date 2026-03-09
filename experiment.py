961c7bdfb526de9e4a5beb72ce3e037fe25b3c6d
Petr Hanák <pe.hanak.tr@gmail.com>
2026-03-09 13:41:00 +0100

    add method component template

--- experiment.py
+++ experiment.py
@@ -5,23 +5,21 @@ from utils.cap_manipulation_utils import generate_cap_for_package_aid
 
 
 cap_file = CapFile.load_from_directory("load_enumeration/class_component/test/javacard")
-cap_file.class_component.pretty_print()
+cap_file.method_component.pretty_print()
 # cap_file.header_component.package.aid = bytearray.fromhex("ffff")
 
-cap_file.export_to_directory("load_enumeration/header_component/test/javacard")
-f = open("load_enumeration/class_component/test/javacard/Class.cap", "rb")
+# cap_file.export_to_directory("load_enumeration/header_component/test/javacard")
+f = open("load_enumeration/method_component/test/javacard/Method.cap", "rb")
 content = bytearray(f.read())
 content[5] = 0xff
 f.close()
 print(content.hex())
 
-fw = open("load_enumeration/class_component/test/javacard/Class.cap", "wb")
+fw = open("load_enumeration/method_component/test/javacard/Method.cap", "wb")
 fw.write(bytes(content))
 fw.close()
-pack_directory_to_cap_file("class_component.cap", "load_enumeration/class_component")
-#
-#
-#
+pack_directory_to_cap_file("method_component.cap", "load_enumeration/method_component")
+
 
 
 
