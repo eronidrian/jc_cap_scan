81a1edcbcf3ac14e5da8ccf99f811b88f05683ae
Petr Hanák <pe.hanak.tr@gmail.com>
2026-03-09 10:08:04 +0100

    new header template

--- experiment.py
+++ experiment.py
@@ -4,7 +4,21 @@ from utils.cap_manipulation_utils import generate_cap_for_package_aid
 
 
 cap_file = CapFile.load_from_directory("templates/generic_template/test/javacard")
-cap_file.header_component.magic = bytearray.fromhex("ffffffff")
+cap_file.header_component.package.aid = bytearray.fromhex("ffff")
 
 cap_file.export_to_directory("load_enumeration/header_component/test/javacard")
 pack_directory_to_cap_file("header_component.cap", "load_enumeration/header_component")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# header magic not checked
