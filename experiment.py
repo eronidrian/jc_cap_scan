3c55d02b32901da153bcfa511d28d14b1af8de1c
Petr Hanák <pe.hanak.tr@gmail.com>
2026-03-09 10:40:52 +0100

    add directory component template

--- experiment.py
+++ experiment.py
@@ -8,14 +8,14 @@ from utils.cap_manipulation_utils import generate_cap_for_package_aid
 # cap_file.header_component.package.aid = bytearray.fromhex("ffff")
 
 # cap_file.export_to_directory("load_enumeration/header_component/test/javacard")
-# f = open("load_enumeration/generic_template/test/javacard/Header.cap", "rb")
-# content = bytearray(f.read())
-# content[2] = 0
-# print(content.hex())
-#
-# fw = open("load_enumeration/generic_template/test/javacard/Header.cap", "wb")
-# fw.write(content)
-pack_directory_to_cap_file("header_component.cap", "load_enumeration/generic_template")
+f = open("load_enumeration/directory_component/test/javacard/Directory.cap", "rb")
+content = bytearray(f.read())
+content[2] = 0
+print(content.hex())
+
+fw = open("load_enumeration/directory_component/test/javacard/Directory.cap", "wb")
+fw.write(content)
+pack_directory_to_cap_file("directory_component.cap", "load_enumeration/directory_component")
 
 
 
