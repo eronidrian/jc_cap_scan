eff01a0a471f827264a5aa93bd777e9c97abf75b
Petr Hanák <pe.hanak.tr@gmail.com>
2026-03-09 12:57:28 +0100

    new class component template

--- experiment.py
+++ experiment.py
@@ -8,16 +8,16 @@ from utils.cap_manipulation_utils import generate_cap_for_package_aid
 # cap_file.header_component.package.aid = bytearray.fromhex("ffff")
 
 # cap_file.export_to_directory("load_enumeration/header_component/test/javacard")
-f = open("load_enumeration/applet_component/test/javacard/Applet.cap", "rb")
+f = open("load_enumeration/class_component/test/javacard/Class.cap", "rb")
 content = bytearray(f.read())
-content[2] = 0x00
+content[9] = 0xff
 f.close()
 print(content.hex())
 
-fw = open("load_enumeration/applet_component/test/javacard/Applet.cap", "wb")
+fw = open("load_enumeration/class_component/test/javacard/Class.cap", "wb")
 fw.write(bytes(content))
 fw.close()
-pack_directory_to_cap_file("applet_component.cap", "load_enumeration/applet_component")
+pack_directory_to_cap_file("class_component.cap", "load_enumeration/class_component")
 
 
 
