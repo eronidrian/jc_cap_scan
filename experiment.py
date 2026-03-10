3bae928575d2b9b35d49263fb703a45c0691f43e
Petr Hanák <pe.hanak.tr@gmail.com>
2026-03-10 09:10:39 +0100

    change javacos config

--- experiment.py
+++ experiment.py
@@ -8,8 +8,8 @@ from utils.cap_manipulation_utils import generate_cap_for_package_aid
 cap_file = CapFile.load_from_directory("templates/generic_template/test/javacard")
 print(cap_file.import_component.packages[0])
 print(cap_file.import_component.packages[1])
-cap_file.import_component.packages.append(PackageInfo(cap_file, 0, 1, bytes.fromhex("A0000000620201")))
-cap_file.export_to_directory("load_enumeration/import_A0000000620201/test/javacard")
+cap_file.import_component.packages.append(PackageInfo(cap_file, 0, 1, bytes.fromhex("A000000062FF01")))
+cap_file.export_to_directory("load_enumeration/import_A000000062FF01/test/javacard")
 
 # cap_file.header_component.package.aid = bytearray.f   romhex("ffff")
 
@@ -23,7 +23,7 @@ cap_file.export_to_directory("load_enumeration/import_A0000000620201/test/javaca
 # fw = open("load_enumeration/staticfield_component/test/javacard/StaticField.cap", "wb")
 # fw.write(bytes(content))
 # fw.close()
-pack_directory_to_cap_file("load_enumeration/import_A0000000620201.cap", "load_enumeration/import_A0000000620201")
+pack_directory_to_cap_file("load_enumeration/import_A000000062FF01.cap", "load_enumeration/import_A000000062FF01")
 
 
 
